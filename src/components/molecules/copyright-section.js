d7cad988eba093605e8795d0aa4a32043787484f
Ramon Pinho <ramonuchoa386@gmail.com>
2022-02-26 10:35:41 -0300

    set author img and link

--- src/components/molecules/copyright-section.js
+++ src/components/molecules/copyright-section.js
@@ -1,37 +1,60 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {useCallback} from 'react';
+import {StyleSheet, Button, Text, View, Image, Linking} from 'react-native';
 
 const styles = StyleSheet.create({
   copyright: {
-      justifyContent: 'flex-start',
-      paddingBottom: 20,
-      paddingLeft: 20,
-      paddingRight: 20
+    justifyContent: 'flex-start',
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
   copyrightText: {
-      fontSize: 16,
-      lineHeight: 16,
-      color: 'white',
-      textTransform: 'uppercase'
+    fontSize: 12,
+    color: 'white',
+    textTransform: 'uppercase',
+    marginBottom: 6,
+  },
+  copyrightSource: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   author: {
-      fontSize: 24,
-      lineHeight: 24,
-  }
+    fontSize: 18,
+    textDecorationLine: 'underline',
+  },
+  profilePic: {
+    width: 22,
+    height: 22,
+    borderRadius: 22,
+    borderColor: 'white',
+    borderWidth: 1,
+    marginEnd: 6,
+  },
 });
- 
-const CopySection = ({authorName}) => {
+
+const CopySection = ({authorName, authorPic, authorUrl}) => {
+  const handlePress = useCallback(async () => {
+    await Linking.openURL(authorUrl);
+  }, [authorUrl]);
+
   return (
     <View style={styles.copyright}>
       <Text style={styles.copyrightText}>Autor:</Text>
-      <Text style={styles.author}>{authorName}</Text>
+      <View style={styles.copyrightSource}>
+        <Image
+          resizeMethod="scale"
+          resizeMode="cover"
+          style={styles.profilePic}
+          source={{
+            uri: authorPic,
+          }}
+        />
+        <Text style={styles.author} onPress={handlePress}>
+          {authorName}
+        </Text>
+      </View>
     </View>
   );
 };
- 
- export default CopySection;
- 
+
+export default CopySection;
