a4604ef101c9f27d5810e7254fbeda02cddc7055
Ramon Pinho <ramonuchoa386@gmail.com>
2022-02-26 10:36:39 -0300

    refactor

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,7 +1,9 @@
 import WeatherApiConfig from '../config/weather-api-config';
 
-export async function getCurrentWeatherData(lat,long) {
-  const url = `${WeatherApiConfig.baseUrl + WeatherApiConfig.currentWeather}?key=${WeatherApiConfig.apiKey}&q=${lat},${long}`
+export async function getCurrentWeatherData(lat, long) {
+  const url = `${
+    WeatherApiConfig.baseUrl + WeatherApiConfig.currentWeather
+  }?key=${WeatherApiConfig.apiKey}&q=${lat},${long}`;
 
   try {
     const response = await fetch(url);
@@ -9,15 +11,17 @@ export async function getCurrentWeatherData(lat,long) {
 
     return {
       temp: json.current.temp_c,
-      query: encodeURI(json.current.condition.text).toLowerCase()
+      query: encodeURI(json.current.condition.text).toLowerCase(),
     };
   } catch (error) {
     console.error(error);
-  };
-};
+  }
+}
 
-export async function getForecastData(lat,long) {
-  const url = `${WeatherApiConfig.baseUrl + WeatherApiConfig.forecast}?key=${WeatherApiConfig.apiKey}&q=${lat},${long}&days=7`
+export async function getForecastData(lat, long) {
+  const url = `${WeatherApiConfig.baseUrl + WeatherApiConfig.forecast}?key=${
+    WeatherApiConfig.apiKey
+  }&q=${lat},${long}&days=7`;
 
   let forecast = [];
 
@@ -29,12 +33,12 @@ export async function getForecastData(lat,long) {
       forecast.push({
         day: element.date,
         max: element.day.maxtemp_c,
-        min: element.day.mintemp_c
+        min: element.day.mintemp_c,
       });
     });
   } catch (error) {
     console.error(error);
   } finally {
     return forecast;
-  };
-};
+  }
+}
