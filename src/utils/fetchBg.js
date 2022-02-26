7f39a74b2e8a657c3bfd35f7d77aed224b0630e9
Ramon Pinho <ramonuchoa386@gmail.com>
2022-02-26 10:36:21 -0300

    refactor

--- src/utils/fetchBg.js
+++ src/utils/fetchBg.js
@@ -2,28 +2,31 @@ import UnsplashKeys from '../config/unsplash-config';
 import sampleImg from '../assets/images/ibrahim-rifath-u2ey1Y1g0dI-unsplash.jpg';
 
 export default async function getBg(query) {
-  const url = `${UnsplashKeys.baseUrl}?client_id=${UnsplashKeys.accessKey}&query=${query}&page=1&per_page=1`;
+  const numberOfImgs = 10;
+  const url = `${UnsplashKeys.baseUrl}?client_id=${UnsplashKeys.accessKey}&query=${query}&page=1&per_page=${numberOfImgs}`;
 
   let bgObj = {
     imgUrl: sampleImg,
     nome: 'Sample Info',
     link: 'https://google.com',
-    profileImg: null
+    profileImg: null,
   };
-  
+
   try {
     const response = await fetch(url);
     const json = await response.json();
 
+    const choosen = json.results[Math.floor(Math.random() * numberOfImgs)];
+
     bgObj = {
-      imgUrl: json.results[0].urls.full,
-      nome: json.results[0].user.name,
-      link: json.results[0].user.links.html,
-      profileImg: json.results[0].user.profile_image.small
+      imgUrl: choosen.urls.full,
+      nome: choosen.user.name,
+      link: choosen.user.links.html,
+      profileImg: choosen.user.profile_image.small,
     };
   } catch (error) {
     console.error(error);
   } finally {
     return bgObj;
   }
-};
+}
